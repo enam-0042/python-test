928338add626a6ab81ea7eba6dd64ecef68df8b0
abir <abdur.rahim@bitmorpher.com>
2025-11-27 12:30:54 +0600

    chore: updating to manged by uv

--- core/log_config.py
+++ core/log_config.py
@@ -1,7 +1,7 @@
-# this code is from gemini as previous code is not working 
+# this code is from gemini as previous code is not working
 
 
-import logging 
+import logging
 from logging.handlers import RotatingFileHandler
 import os
 
@@ -9,32 +9,36 @@ APP_LOGGER_NAME = "Fastapi_Universal_api"
 
 logger = logging.getLogger(APP_LOGGER_NAME)
 logger.setLevel(logging.DEBUG)
+LOG_DIR = "log"
+os.makedirs(
+    LOG_DIR, exist_ok=True
+)  # This will create the directory if it doesn't exist
 
 if not logger.handlers:
     log_format = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"        
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    console_handler  = logging.StreamHandler()
+    console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(log_format)
     logger.addHandler(console_handler)
 
-    log_file_path = "log/app.log"
+    log_file_path = f"{LOG_DIR}/app.log"
 
     file_handler = RotatingFileHandler(
-        log_file_path,
-        maxBytes=5*1024*1024,
-        backupCount=5
+        log_file_path, maxBytes=5 * 1024 * 1024, backupCount=5
     )
 
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(log_format)
     logger.addHandler(file_handler)
 
-    logger.info("Global logging configuration initialized (Console: INFO+, File: DEBUG+)")
+    logger.info(
+        "Global logging configuration initialized (Console: INFO+, File: DEBUG+)"
+    )
+
 
 def get_logger():
     return logger
-
